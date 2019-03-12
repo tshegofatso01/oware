f2d7ded7778b852e598256ee17dd2a1e5cb4e689
YMM <cinyc.s@gmail.com>
2019-03-12 16:15:20 +0200

    Program.fs initial version

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,5 +1,19 @@
 ﻿module Oware
 
+type StartingPosition =
+    | South
+    | North
+
+let getSeeds n board = failwith "Not implemented"
+
+let useHouse n board = failwith "Not implemented"
+
+let start position = failwith "Not implemented"
+
+let score board = failwith "Not implemented"
+
+let gameState board = failwith "Not implemented"
+
 [<EntryPoint>]
 let main _ =
     printfn "Hello from F#!"
